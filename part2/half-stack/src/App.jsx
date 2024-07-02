93cbabdbc7accc9e6de8edb2dd3a7536b0a0c072
ZmijaZ <zmijaz2001@gmail.com>
2024-07-02 13:12:56 +0200

    Setup for exercises 2.1-2.5

---
+++ part2/half-stack/src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <>
+      <h1>Half stack</h1>
+    </>
+  )
+}
+
+export default App
