67ce0ab2f57721b334e13f37057531455f3d7f7b
ZmijaZ <zmijaz2001@gmail.com>
2024-07-02 12:18:05 +0200

    Completed exercise 1.10

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -12,7 +12,7 @@ const Button = (props) => {
   )
 }
 
-const Part = ({content}) => {
+const StatisticsLine = ({content}) => {
   return (
     <p>{content.name} {content.count}</p>
   )
@@ -24,45 +24,81 @@ const Statistics = ({data}) => {
   else
     return (
       <>
-        <Part content = {data.content[0]}></Part>
-        <Part content = {data.content[1]}></Part>
-        <Part content = {data.content[2]}></Part>
-        <Part content = {data.content[3]}></Part>
-        <Part content = {data.content[4]}></Part>
-        <Part content = {data.content[5]}></Part>
+        <StatisticsLine content = {data.content[0]}/>
+        <StatisticsLine content = {data.content[1]}/>
+        <StatisticsLine content = {data.content[2]}/>
+        <StatisticsLine content = {data.content[3]}/>
+        <StatisticsLine content = {data.content[4]}/>
+        <StatisticsLine content = {data.content[5]}/>
       </>
     )
 }
 
 function App() {
 
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
-  const [score, setScore] = useState(0)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+  // const [good, setGood] = useState(0)
+  // const [neutral, setNeutral] = useState(0)
+  // const [bad, setBad] = useState(0)
+  // const [all, setAll] = useState(0)
+  // const [score, setScore] = useState(0)
+  // const [average, setAverage] = useState(0)
+  // const [positive, setPositive] = useState(0)
+  const [statistics, setStatistics] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+    all: 0,
+    score: 0,
+    average: 0,
+    positive: 0,
+
+  })
 
   const handleGood = () => {
-    setGood(good + 1)
-    setAll(all + 1)
-    setScore(score + 1)
-    setAverage((score + 1) / (all + 1))
-    setPositive((good + 1) / (all + 1))
+    setStatistics(
+      {...statistics,
+        good: statistics.good + 1,
+        all: statistics.all + 1,
+        score: statistics.score + 1,
+        average: (statistics.score + 1) / (statistics.all + 1),
+        positive: (statistics.good + 1) / (statistics.all + 1)
+      }
+    )
+    // setGood(good + 1)
+    // setAll(all + 1)
+    // setScore(score + 1)
+    // setAverage((score + 1) / (all + 1))
+    // setPositive((good + 1) / (all + 1))
   }
   const handleNeutral = () => {
-    setNeutral(neutral + 1)
-    setAll(all + 1)
-    setAverage((score) / (all + 1))
-    setPositive((good) / (all + 1))
+    setStatistics(
+      {...statistics,
+        neutral: statistics.neutral + 1,
+        all: statistics.all + 1,
+        average: (statistics.score) / (statistics.all + 1),
+        positive: (statistics.good) / (statistics.all + 1)
+      }
+    )
+    // setNeutral(neutral + 1)
+    // setAll(all + 1)
+    // setAverage((score) / (all + 1))
+    // setPositive((good) / (all + 1))
   }
   const handleBad = () => {
-    setBad(bad + 1)
-    setAll(all + 1)
-    setScore(score - 1)
-    setAverage((score - 1) / (all + 1))
-    setPositive((good) / (all + 1))
+    setStatistics(
+      {...statistics,
+        bad: statistics.bad + 1,
+        all: statistics.all + 1,
+        score: statistics.score - 1,
+        average: (statistics.score - 1) / (statistics.all + 1),
+        positive: (statistics.good) / (statistics.all + 1)
+      }
+    )
+    // setBad(bad + 1)
+    // setAll(all + 1)
+    // setScore(score - 1)
+    // setAverage((score - 1) / (all + 1))
+    // setPositive((good) / (all + 1))
   }
 
   const data = {
@@ -76,27 +112,27 @@ function App() {
     content: [
       {
         name: "good",
-        count: good
+        count: statistics.good
       },
       {
         name: "neutral",
-        count: neutral
+        count: statistics.neutral
       },
       {
         name: "bad",
-        count: bad
+        count: statistics.bad
       },
       {
         name: "all",
-        count: all
+        count: statistics.all
       },
       {
         name: "average",
-        count: average
+        count: statistics.average
       },
       {
         name: "positive",
-        count: positive
+        count: statistics.positive
       }
 
     ]
