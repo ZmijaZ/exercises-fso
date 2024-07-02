d5d209d8944b6c7c440feb8481ae4f688c809c56
ZmijaZ <zmijaz2001@gmail.com>
2024-07-02 14:19:48 +0200

    Completed exercise 2.5

--- part2/half-stack/src/App.jsx
+++ part2/half-stack/src/App.jsx
@@ -1,53 +1,5 @@
 import { useState } from 'react'
-
-const Header = ({text}) => {
-  return (
-    <h2>{text}</h2>
-  )
-}
-
-const Content = ({parts}) => {
-  return(
-    <div>
-      {parts.map(part => {
-        return(
-          <Part key = {part.id}part = {part}></Part>
-        )
-      })}
-    </div>
-  )
-}
-
-const Part = ({part}) => {
-  return(
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = ({parts}) => {
-  const values = parts.map(part => part.exercises)
-  return (
-    <strong>total of {values.reduce((acc, curr) => acc + curr, 0)} exercises</strong>
-  )
-}
-
-const Course = ({course}) => { 
-  return(
-    <div>
-      <Header text = {course.name}/>
-      <Content parts = {course.parts}/>
-      <Total parts = {course.parts}/>
-    </div>
-  )
-}
-
-const AllCourses = ({courses}) => {
-  return (
-    <div>
-      {courses.map(course => <Course key = {course.id} course = {course}/>)}
-    </div>
-  )
-}
+import AllCourses from './components/Course'
 
 function App() {
   const courses = [
