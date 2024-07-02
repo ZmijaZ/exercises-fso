6f013060ec8bd84858771dbc55f3da55cd5772e6
ZmijaZ <zmijaz2001@gmail.com>
2024-07-02 11:28:55 +0200

    Setup for exercises 1.6-1.*

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,72 +1,24 @@
 import { useState } from 'react'
 
-const Header = ({course}) => {
+const Header = ({text}) => {
   return (
-    <h1>{course}</h1>
+    <h1>{text}</h1>
   )
 }
 
-const Content = (props) => {
-  return (
-    <>
-      <Part part = {props.part1}></Part>
-      <Part part = {props.part2}></Part>
-      <Part part = {props.part3}></Part>
-    </>    
-  )
-}
-
-const Part = ({part}) => {
-  return(
-    <p>{part.name} {part.exercises}</p>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <h1>{props.exercises1 + props.exercises2 + props.exercises3}</h1>
-  )
-}
 
 function App() {
 
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
-
-
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
-    <>
-      
-      <Header course = {course.name}></Header>
-      <Content 
-        part1={course.parts[0]} 
-        part2={course.parts[1]} 
-        part3={course.parts[2]} 
-        ></Content>
-      <Total
-        exercises1 = {course.parts[0].exercises} 
-        exercises2 = {course.parts[1].exercises} 
-        exercises3 = {course.parts[2].exercises}>
-      </Total>
-    </>
+    <div>
+      code here
+    </div>
   )
+
 }
 
 export default App
