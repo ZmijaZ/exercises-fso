8af8863aec999ea50d456dd0e03c7161e79dd441
ZmijaZ <zmijaz2001@gmail.com>
2024-07-02 11:58:43 +0200

    Completed exercise 1.7

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -24,6 +24,9 @@ const Content = ({data}) => {
       <Part content = {data.content[0]}></Part>
       <Part content = {data.content[1]}></Part>
       <Part content = {data.content[2]}></Part>
+      <Part content = {data.content[3]}></Part>
+      <Part content = {data.content[4]}></Part>
+      <Part content = {data.content[5]}></Part>
     </>
   )
 }
@@ -33,15 +36,30 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [score, setScore] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
   const handleGood = () => {
     setGood(good + 1)
+    setAll(all + 1)
+    setScore(score + 1)
+    setAverage((score + 1) / (all + 1))
+    setPositive((good + 1) / (all + 1))
   }
   const handleNeutral = () => {
     setNeutral(neutral + 1)
+    setAll(all + 1)
+    setAverage((score) / (all + 1))
+    setPositive((good) / (all + 1))
   }
   const handleBad = () => {
     setBad(bad + 1)
+    setAll(all + 1)
+    setScore(score - 1)
+    setAverage((score - 1) / (all + 1))
+    setPositive((good) / (all + 1))
   }
 
   const data = {
@@ -64,7 +82,20 @@ function App() {
       {
         name: "bad",
         count: bad
+      },
+      {
+        name: "all",
+        count: all
+      },
+      {
+        name: "average",
+        count: average
+      },
+      {
+        name: "positive",
+        count: positive
       }
+
     ]
   }
 
