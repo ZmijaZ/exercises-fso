5b94a32a358621a0afe317d3ff170ccd7458453c
ZmijaZ <zmijaz2001@gmail.com>
2024-07-02 14:04:59 +0200

    Completed exercise 2.1

--- part2/half-stack/src/App.jsx
+++ part2/half-stack/src/App.jsx
@@ -1,35 +1,69 @@
 import { useState } from 'react'
 
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Content = ({parts}) => {
+  return(
+    <div>
+      <Part part = {parts[0]}/>
+      <Part part = {parts[1]}/>
+      <Part part = {parts[2]}/>
+    </div>
+  )
+}
+
+const Part = ({part}) => {
+  return(
+    <p>{part.name} {part.exercises}</p>
+  )
+}
+
+const Total = ({parts}) => {
+  return (
+    <p>{parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+  )
+}
+
+const Course = ({course}) => { 
+  return(
+    <div>
+      <Header text = {course.name}/>
+      <Content parts = {course.parts}/>
+      <Total parts = {course.parts}/>
+    </div>
+  )
+
+}
 
 function App() {
-
   const course = {
-    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+        exercises: 10
       },
       {
         name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
+        exercises: 7
       },
       {
         name: 'State of a component',
-        exercises: 14,
-        id: 3
+        exercises: 14
       }
     ]
   }
 
   return (
     <>
-      <h1>Half stack</h1>
+      <Course course = {course}/>
     </>
   )
+
 }
 
 export default App
